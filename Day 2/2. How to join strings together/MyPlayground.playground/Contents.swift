9cfcc1546dd490ebddb3188c57b970fb0b05cf81
Ayon Khan <ayonkhan@gmail.com>
2023-05-25 21:53:49 -0600

    Complete day 2

---
+++ Day 2/2. How to join strings together/MyPlayground.playground/Contents.swift
@@ -0,0 +1,24 @@
+import Cocoa
+
+let firstPart = "Hello, "
+let secondPart = "world!"
+let greeting = firstPart + secondPart
+
+let people = "Haters"
+let action = "hate"
+let lyric = people + " gonna " + action
+print(lyric)
+
+let luggageCode = "1" + "2" + "3" + "4" + "5"
+
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+let name = "Taylor"
+let age = 26
+let message = "Hello, my name is \(name) and I'm \(age) years old."
+print(message)
+
+let number = 11
+let missionMessage = "Apollo \(number) landed on the moon."
+
+print("5 x 5 is \(5 * 5)")
---
+++ Day 2/Checkpoint 1/MyPlayground.playground/Contents.swift
@@ -0,0 +1,6 @@
+import Cocoa
+
+let celsius = 16.0
+let fahrenheit = ((celsius * 9) / 5) + 32
+
+print("\(celsius)°C = \(fahrenheit)°F")
