9d1be210607a1adb074b7924b857759bd7d0d19f
Ayon Khan <ayonkhan@gmail.com>
2023-05-26 23:33:25 -0600

    Complete day 3

---
+++ Day 3/3. How to use sets for fast data lookup/MyPlayground.playground/Contents.swift
@@ -0,0 +1,16 @@
+import Cocoa
+
+//let actors = Set([
+//    "Denzel Washington",
+//    "Tom Cruise",
+//    "Nicolas Cage",
+//    "Samuel L Jackson"
+//])
+//print(actors)
+
+var actors = Set<String>()
+actors.insert("Denzel Washington")
+actors.insert("Tom Cruise")
+actors.insert("Nicolas Cage")
+actors.insert("Samuel L Jackson")
+print(actors)
---
+++ Day 3/4. How to create and use enums/MyPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+import Cocoa
+
+var selected = "Monday"
+selected = "Tuesday"
+selected = "January"
+selected = "Friday "
+
+enum Weekday {
+    case monday, tuesday, wednesday, thursday, friday
+}
+
+var day = Weekday.monday
+day = .tuesday
+day = .friday
