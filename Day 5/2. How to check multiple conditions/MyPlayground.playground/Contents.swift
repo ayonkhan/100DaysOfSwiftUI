b1af9df650b87dee70393d32cd43b9e86a858256
Ayon Khan <ayonkhan@gmail.com>
2023-05-28 23:58:44 -0600

    Complete day 5

---
+++ Day 5/2. How to check multiple conditions/MyPlayground.playground/Contents.swift
@@ -0,0 +1,55 @@
+import Cocoa
+
+let age = 16
+
+if age >= 18 {
+    print("You can vote in the next election.")
+} else {
+    print("Sorry, you're too young to vote.")
+}
+
+let a = false
+let b = false
+
+if a {
+    print("Code to run if a is true")
+} else if b {
+    print("Code to run if a is false and b is true")
+} else {
+    print("Code to run if both a and b are false")
+}
+
+let temp = 25
+
+if temp > 20 {
+    if temp < 30 {
+        print("It's a nice day.")
+    }
+}
+
+if temp > 20 && temp < 30 {
+    print("It's a nice day.")
+}
+
+let userAge = 14
+let hasParentalConsent = true
+
+if age >= 18 || hasParentalConsent {
+    print("You can buy the game!")
+}
+
+enum TransportOption {
+    case airplane, helicopter, bicycle, car, scooter
+}
+
+let transport = TransportOption.airplane
+
+if transport == .airplane || transport == .helicopter {
+    print("Let's fly!")
+} else if transport == .bicycle {
+    print("I hope there's a bike path...")
+} else if transport == .car {
+    print("Time to get stuck in traffic.")
+} else {
+    print("I'm going to hire a scooter now!")
+}
