578822da9c44b4062425d573402ff288406d9dbb
Ayon Khan <ayonkhan@gmail.com>
2023-06-04 23:44:15 -0600

    Complete day 10

---
+++ Day 10/4. How to create custom initializers/MyPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+import Cocoa
+
+struct Player {
+    let name: String
+    let number: Int
+    
+    init(name: String) {
+        self.name = name
+        number = Int.random(in: 1...99)
+    }
+}
+
+let player = Player(name: "Megan R")
+print(player.number)
